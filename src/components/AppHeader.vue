af4153f5b8e20fe5052761d23b393d29fe4fc625
Tpakmopucm <explay0907@gmail.com>
2022-04-28 22:25:56 +0500

    First commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">Marvel</a>
+        <form class="d-flex">
+            <input type="search"
+                    placeholder="Поиск..."
+                    aria-label="Search"
+                    class="form-control me-2"
+                    v-model="search"
+                    @input="characterSearch(search)"
+                    @keypress.enter.prevent
+            >
+            <button
+                    type="reset"
+                    class="btn btn-outline-light"
+                    @click="search = ''; characterSearch(search)"
+            >
+                все
+            </button>
+        </form>
+    </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ['characterSearch'],
+        data() {
+            return {
+                search: '',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
